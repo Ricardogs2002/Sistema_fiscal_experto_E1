a55e63ea11485c1f012dfa9e1b34b74c2d8e6152
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-12-12 01:48:27 -0600

    Update Fisico CJQI_002.java

--- Sistema experto java/Fisico.java
+++ Sistema experto java/Fisico.java
@@ -1,60 +1,88 @@
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.awt.event.*;
-import java.awt.Font;
 import java.awt.*;
 import java.io.*;
-class Fisico extends JPanel implements ActionListener{
+class Fisico extends JPanel implements ActionListener, ItemListener{
     Font fuente=new Font("Algerian",Font.BOLD,20);
     Font fuente1=new Font("Algerian",Font.PLAIN,20);
     Font fuente2=new Font("Arial",Font.PLAIN,18);
+    Fisico_Moral Ventana3;
     //public JTextArea resumen;
     public JButton botonF1,botonF2,botonF3,botonA;//agregar tantos botones Fx como sea necesario
+    private JComboBox<String> opciones;
     //public JScrollPane scroll;
     public JScrollPane Paneles;
     public Fisico(){
+
         setLayout(null);
         setBackground(new Color(200,242,144));
-        botonF1= new JButton("Fisica 1");
-        botonF1.setBounds(105,100,150,30);
-        botonF1.setFont(fuente2);
-        botonF2 = new JButton("Fisica 2");
-        botonF2.setBounds(105,150,150,30);
-        botonF2.setFont(fuente2);
-        botonF3 = new JButton("Fisica 3");
-        botonF3.setBounds(105,200,150,30);
-        botonF3.setFont(fuente2);
+        opciones = new JComboBox();
+        opciones.setBounds(105,150,150,30);
+        opciones.addItem("Incorporación fiscal");
+        opciones.addItem("Actividades empresariales");
+        opciones.addItem("Arrendamiento de inmuebles");
+        opciones.addItem("Servicios profesionales");
+        opciones.addItem("Asalariado");
+
+
         botonA = new JButton("Atras");
         botonA.setBounds(105,480,150,30);
         botonA.setFont(fuente2);
+        botonF1 = new JButton("Siguiente");
+        botonF1.setBounds(105,300,150,30);
+        botonF1.setFont(fuente2);
         Paneles = new JScrollPane();
         Paneles.setBounds(0,0,800,600);
         Paneles.setVisible(false);
-        add(botonF1);
-        add(botonF2);
-        add(botonF3);
         add(botonA);
+        add(botonF1);
         add(Paneles);
-        botonF1.addActionListener(this);
-        botonF2.addActionListener(this);
-        botonF3.addActionListener(this);
+        add(opciones);
         botonA.addActionListener(this);
+        botonF1.addActionListener(this);
 
 
 //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
     }
     public void actionPerformed(ActionEvent accion){
-        if (accion.getSource() == botonF1){
-            Paneles.setVisible(true);
-        }
-        if (accion.getSource() == botonF2){
-        }
-        if (accion.getSource() == botonF3){
-        }
         if (accion.getSource() == botonA){
+            Paneles.setVisible(false);
+            Ventana3 = new Fisico_Moral();
+            ver3(Ventana3);
+        }
+        if (accion.getSource() == botonF1){
+            String OPC = (String)opciones.getSelectedItem();
+            if (OPC == "Incorporación fiscal"){
+                System.out.println("1");
+            }
+            if (OPC == "Actividades empresariales"){
+                System.out.println("2");
+            }
+            if (OPC == "Arrendamiento de inmuebles"){
+                System.out.println("3");
+            }
+            if (OPC == "Servicios profesionales"){
+                System.out.println("4");
+            }
+            if (OPC == "Asalariado"){
+                System.out.println("5");
+            }
+
         }
     }
 
+private void ver3(Fisico_Moral coso){
+    Paneles.setViewportView(coso);
+    Play();
+}
+
+private void Play(){
+    botonA.setVisible(false);
+    botonF1.setVisible(false);
+    Paneles.setVisible(false);
+    opciones.setVisible(false);
+}
 //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||    
     //Cambio de fondo
     private Image backgroundImage;
@@ -74,4 +102,10 @@ class Fisico extends JPanel implements ActionListener{
         base.setBounds(150,100,800,600);
         base.setVisible(true);
     }
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'itemStateChanged'");
+    }
 }
+
