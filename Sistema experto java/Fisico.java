c325fa9f2db98b47a96aacd081cf7934e4fcbf9c
JairQuezada <127467227+JairQuezada@users.noreply.github.com>
2023-12-12 02:31:23 -0600

    CJQI_002

--- Sistema experto java/Fisico.java
+++ Sistema experto java/Fisico.java
@@ -11,6 +11,7 @@ class Fisico extends JPanel implements ActionListener, ItemListener{
     //public JTextArea resumen;
     public JButton botonF1,botonF2,botonF3,botonA;//agregar tantos botones Fx como sea necesario
     private JComboBox<String> opciones;
+    public JLabel titulo;
     //public JScrollPane scroll;
     public JScrollPane Paneles;
     public Fisico(){
@@ -36,10 +37,16 @@ class Fisico extends JPanel implements ActionListener, ItemListener{
         Paneles = new JScrollPane();
         Paneles.setBounds(0,0,800,600);
         Paneles.setVisible(false);
+        titulo = new JLabel("Persona Fisica");
+        titulo.setBounds(125,20,450,50); //x, y, ancho, alto. Dentro del frame
+        titulo.setFont(fuente2);
+        titulo.setForeground(Color.WHITE);
         add(botonA);
         add(botonF1);
         add(Paneles);
         add(opciones);
+        add(titulo);
+        
         botonA.addActionListener(this);
         botonF1.addActionListener(this);
 
@@ -54,20 +61,23 @@ class Fisico extends JPanel implements ActionListener, ItemListener{
         }
         if (accion.getSource() == botonF1){
             String OPC = (String)opciones.getSelectedItem();
+            if (OPC == "Selecciona una opcion"){
+                JOptionPane.showMessageDialog(null, "Seleccione alguna opcion","", JOptionPane.WARNING_MESSAGE);
+            }
             if (OPC == "Incorporación fiscal"){
-                System.out.println("1");
+                JOptionPane.showMessageDialog(null, "Podrán ser contribuyentes a este tipo de régimen las personas que realizan actividades empresariales u ofrezcan servicios donde no sea obligatorio un título profesional. \nEjemplo: fruterías, talleres mecánicos, salones de belleza, papelería, tintorerías y todas aquellas que obtengan ingresos que no sobrepasen los dos millones de peso anual.");
             }
             if (OPC == "Actividades empresariales"){
-                System.out.println("2");
+                JOptionPane.showMessageDialog(null,"En este tipo, no se establece un monto mínimo de ingreso y es obligatorio que se registren en este régimen, las personas físicas que realicen actividades industriales, de transporte, comerciales, ganaderas, agrícolas o de pesca. \nEjemplo: restaurantes, ferreterías, guarderías, bares, artesanos, entre otros." );
             }
             if (OPC == "Arrendamiento de inmuebles"){
-                System.out.println("3");
+                JOptionPane.showMessageDialog(null,"Pertenecen a este régimen aquella personas que obtengan ingresos mensuales o anuales por alquilar o rentar departamentos, habitaciones, locales comerciales y cualquier bien mueble del que sea poseedor.");
             }
             if (OPC == "Servicios profesionales"){
-                System.out.println("4");
+                JOptionPane.showMessageDialog(null,"Si eres profesional de la salud y acabas de abrir un consultorio, te desempeñas como freelance u obtienes un sueldo mensual por prestar tus conocimientos profesionales de forma independiente, \nes decir, sin depender de una empresa cobrando como asalariado, perteneces a este tipo de régimen fiscal.");
             }
             if (OPC == "Asalariado"){
-                System.out.println("5");
+                JOptionPane.showMessageDialog(null, "Son los que trabajan en subordinación a una organización o empresa. Estos sólo hacen una declaración anual ante la hacienda pública." );
             }
 
         }
